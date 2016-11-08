561d9d5dc594959570adcb31df8cc7d5050bf9a8
tofid <andreyklochok@gmail.com>
2016-11-08 19:33:27 +0200

    Added check if .table-responsive has horizontal scroll and added to .dropdown-menu position relative.

--- src/widgets/MenuButton.php
+++ src/widgets/MenuButton.php
@@ -15,8 +15,24 @@ class MenuButton extends \yii\base\Widget
 
     public function run()
     {
+        $this->getView()->registerCss("
+        @media (max-width: 767px) {
+            .menu-button .dropdown-menu {
+                position: relative!important;
+            }
+        }
+        ");
+        $this->getView()->registerJs("
+        $.fn.hasScrollBar = function() {
+            return this.get(0).scrollWidth > this.width();
+        }
+        if ($('div.table-responsive').hasScrollBar() === true) {
+           $('.menu-button .dropdown-menu').css('position', 'relative'); 
+        }
+        ");
+
         $class = $this->menuClass;
-        $html = Html::beginTag('div', ['class' => 'dropdown visible-lg-inline']);
+        $html = Html::beginTag('div', ['class' => 'menu-button dropdown visible-lg-inline visible-md-inline visible-sm-inline visible-xs-inline']);
         $html .= Html::button($this->icon, [
             'class' => 'btn btn-default btn-xs dropdown-toggle',
             'data-toggle' => 'dropdown',
